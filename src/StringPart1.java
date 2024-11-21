3a59b7194b33c649829d6108957bab585ff78e4b
nurmukhammad.yu <nurmukhammad.yo@payme.uz>
2024-11-21 16:39:47 +0500

    without2()

--- src/StringPart1.java
+++ src/StringPart1.java
@@ -1,9 +1,19 @@
 public class StringPart1 {
 
     public String extraFront(String str) {
-        if(str.length() < 2) return str + str + str;
+        if (str.length() < 2) return str + str + str;
 
-        return str.substring(0,2) + str.substring(0,2) + str.substring(0,2);
+        return str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2);
+    }
+
+    public String without2(String str) {
+
+        if (str.length() <= 1) return str;
+
+        if (str.substring(0, 2).equals(str.substring(str.length() - 2)))
+            return str.substring(2);
+        else
+            return str;
     }
 
 }
