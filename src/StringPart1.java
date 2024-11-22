6c9feac574115f207ff903536513fd15cb13d53b
nurmukhammad.yu <nurmukhammad.yo@payme.uz>
2024-11-22 10:32:16 +0500

    endsLy

--- src/StringPart1.java
+++ src/StringPart1.java
@@ -55,4 +55,10 @@ public class StringPart1 {
 
         else return str.substring((str.length()/2)-1, (str.length()/2)+2);
     }
+
+    public boolean endsLy(String str) {
+        if(str.length() < 2) return false;
+
+        return str.endsWith("ly");
+    }
 }
