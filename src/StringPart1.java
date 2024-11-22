b97c6891c40f1913b89c16387c9a33ec38df9970
nurmukhammad.yu <nurmukhammad.yo@payme.uz>
2024-11-22 18:25:57 +0500

    theEnd

--- src/StringPart1.java
+++ src/StringPart1.java
@@ -85,4 +85,12 @@ public class StringPart1 {
 
         return str.substring(1,str.length()-1);
     }
+
+    public String theEnd(String str, boolean front) {
+        if(front) {
+            return str.substring(0,1);
+        } else {
+            return str.substring(str.length()-1);
+        }
+    }
 }
