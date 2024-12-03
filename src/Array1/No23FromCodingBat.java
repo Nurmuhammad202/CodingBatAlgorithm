def926623934946f677683781f3bef0b22372bda
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-03 15:11:08 +0500

    no23

---
+++ src/Array1/No23FromCodingBat.java
@@ -0,0 +1,9 @@
+package Array1;
+
+public class No23FromCodingBat {
+
+    public boolean no23(int[] nums) {
+        return !(nums[0] == 2 || nums[1] == 2 || nums[0] == 3 || nums[1] == 3 );
+    }
+
+}
