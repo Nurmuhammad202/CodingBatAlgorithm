47712885978dd7c0b16b73e673338034b7c50615
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-03 15:22:29 +0500

    double23

---
+++ src/Array1/Double23FromCdingBat.java
@@ -0,0 +1,9 @@
+package Array1;
+
+public class Double23FromCdingBat {
+    public boolean double23(int[] nums) {
+        if(nums.length<2) return false;
+
+        return ((nums[0]==2 && nums[1]==2) ||(nums[0]==3 && nums[1]==3));
+    }
+}
