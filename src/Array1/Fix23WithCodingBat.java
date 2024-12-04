d67286b4c6a99f9ff12e740d68eb2ae4e72ec2e2
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-04 14:51:34 +0500

    Fix23WithCodingBat

--- src/Array1/Fix23WithCodingBat.java
+++ src/Array1/Fix23WithCodingBat.java
@@ -1,7 +1,15 @@
 package Array1;
 
 public class Fix23WithCodingBat {
-    public int fix23(int[] nums) {
-        return 0;
+    public int[] fix23(int[] nums) {
+        for(int i = 0; i<nums.length-1; i++) {
+            if(i+1 <= nums.length -1) {
+                if (nums[i] == 2 && nums[i + 1] == 3) {
+                    nums[i + 1] = 0;
+                }
+            }
+        }
+
+        return nums;
     }
 }
