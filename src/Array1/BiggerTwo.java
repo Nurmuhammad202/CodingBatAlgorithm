89e3a15feb19bef036026c50d6c73eca3a57ae0d
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-04 15:07:09 +0500

    Fix23WithCodingBat

---
+++ src/Array1/BiggerTwo.java
@@ -0,0 +1,18 @@
+package Array1;
+
+import java.util.Arrays;
+
+public class BiggerTwo {
+
+    public int[] biggerTwo(int[] a, int[] b) {
+      int resultA =  Arrays.stream(a).sum();
+      int resultB =  Arrays.stream(b).sum();
+
+      if(resultA>resultB) {
+          return a;
+      } else {
+          return b;
+      }
+
+    }
+}
