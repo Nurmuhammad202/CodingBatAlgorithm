996d1bb30d20ead0957f789bd93be3dc675db69b
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-09 15:03:43 +0500

    AeenSum

---
+++ src/Logic1/AsnwerCall.java
@@ -0,0 +1,13 @@
+package Logic1;
+
+public class AsnwerCall {
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        // If you're asleep, do not answer
+        if (isAsleep) {
+            return false;
+        }
+        // If it's morning, only answer if it's your mom
+        return !isMorning || isMom;
+        // Otherwise, answer
+    }
+}
