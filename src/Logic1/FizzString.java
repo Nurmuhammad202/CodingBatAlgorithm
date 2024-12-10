e51f283029aa51a53bd1359c3dcc629b9af11115
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-10 16:36:58 +0500

    fizzString

---
+++ src/Logic1/FizzString.java
@@ -0,0 +1,21 @@
+package Logic1;
+
+public class FizzString {
+    public String fizzString(String str) {
+        if(!str.startsWith("f") && !str.endsWith("b")) {
+            return str;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if(str.startsWith("f")) {
+            stringBuilder.append("Fizz");
+        }
+
+        if(str.endsWith("b")) {
+            stringBuilder.append("Buzz");
+        }
+
+        return stringBuilder.toString();
+    }
+}
