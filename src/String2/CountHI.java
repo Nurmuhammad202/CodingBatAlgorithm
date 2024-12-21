4089de9b2ba61dc01768ed7a0210bd6dcb3b3ba7
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-21 17:07:45 +0500

    makeChocolate

---
+++ src/String2/CountHI.java
@@ -0,0 +1,20 @@
+package String2;
+
+public class CountHI {
+    public int countHi(String str) {
+
+        if(str.length() <= 1) return 0;
+        char[] list = str.toCharArray();
+
+        int counter = 0;
+        for (int i = 0; i < list.length; i++) {
+            if ((i + 1) <= list.length) {
+                if(list[i] =='h' && list[i+1] == 'i') {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
