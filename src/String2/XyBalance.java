990ba9691909ac8cf731f0fa8f0703f7a7e143f3
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-25 17:22:01 +0500

    makeChocolate

---
+++ src/String2/XyBalance.java
@@ -0,0 +1,19 @@
+package String2;
+
+public class XyBalance {
+    public static boolean xyBalance(String str) {
+        if(str.isEmpty()) return true;
+
+        if(!str.contains("x")) return true;
+        else {
+            int index = str.lastIndexOf('x');
+            String newItem = str.substring(index);
+            return newItem.contains("y");
+        }
+    }
+
+    public static void main(String[] args) {
+        xyBalance("aaxbby");
+    }
+}
+
