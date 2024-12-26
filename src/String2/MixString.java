c99da65087dda6d1fb7bf9ab26212d8a74f253b6
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-26 18:58:26 +0500

    makeChocolate

---
+++ src/String2/MixString.java
@@ -0,0 +1,20 @@
+package String2;
+
+import java.util.ArrayList;
+
+public class MixString {
+    public String mixString(String a, String b) {
+        ArrayList list = new ArrayList();
+
+        char[] aList = a.toCharArray();
+        char[] bList = b.toCharArray();
+
+        for (int i = 0; i < a.length() + b.length(); i++) {
+            if(aList.length > i) {
+
+            }
+        }
+
+        return "sa";
+    }
+}
