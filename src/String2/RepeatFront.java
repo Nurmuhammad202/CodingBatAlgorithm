c21f7665f15aa5dedf164df2347c0427112f2b36
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2024-12-28 16:35:31 +0500

    makeChocolate

---
+++ src/String2/RepeatFront.java
@@ -0,0 +1,17 @@
+package String2;
+
+public class RepeatFront {
+    public String repeatFront(String str, int n) {
+        String item = str.substring(0, n);
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(item);
+        builder.append(item);
+
+        return builder.toString();
+    }
+
+    private String item() {
+        return "s";
+    }
+}
