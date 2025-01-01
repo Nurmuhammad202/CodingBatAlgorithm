931650520612af7b51b37604a78971ae2a055acd
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-01 17:51:00 +0500

    repeatFront

--- src/String2/RepeatFront.java
+++ src/String2/RepeatFront.java
@@ -2,16 +2,14 @@ package String2;
 
 public class RepeatFront {
     public String repeatFront(String str, int n) {
-        String item = str.substring(0, n);
         StringBuilder builder = new StringBuilder();
 
-        builder.append(item);
-        builder.append(item);
+        String item = str.substring(0,n);
+
+        for (int i = item.length(); i > 0; i--) {
+            builder.append(item.substring(0,i));
+        }
 
         return builder.toString();
-    }
-
-    private String item1() {
-        return "e32";
     }
 }
