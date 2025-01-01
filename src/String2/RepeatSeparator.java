db1b06c977904f9d585d826c8221a798e08fc2fa
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-01 17:56:43 +0500

    repeatSeparator

---
+++ src/String2/RepeatSeparator.java
@@ -0,0 +1,16 @@
+package String2;
+
+public class RepeatSeparator {
+    public String repeatSeparator(String word, String sep, int count) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            builder.append(word);
+            if(count-1 != i) {
+                builder.append(sep);
+            }
+        }
+
+        return builder.toString();
+    }
+}
