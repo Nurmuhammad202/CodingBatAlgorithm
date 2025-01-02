564156ad4065d3fc494de1b4f0741c75b4b61fc2
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-02 15:16:09 +0500

    prefixAgain

---
+++ src/Map/MapShare.java
@@ -0,0 +1,18 @@
+package Map;
+
+import java.util.Map;
+
+public class MapShare {
+    public Map<String, String> mapShare(Map<String, String> map) {
+        // Check if the key "a" exists and has a value
+        if (map.containsKey("a") && map.get("a") != null && !map.get("a").isEmpty()) {
+            // Set the value of key "b" to the value of key "a"
+            map.put("b", map.get("a"));
+        }
+
+        if(map.containsKey("c")) {
+            map.remove("c");
+        }
+        return map;
+    }
+}
