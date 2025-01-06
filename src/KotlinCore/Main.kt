d08ff98651edac1f54e5aec8eee04595bc354fbf
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-06 10:10:19 +0500

    prefixAgain

--- src/KotlinCore/Main.kt
+++ src/KotlinCore/Main.kt
@@ -1,65 +1,11 @@
 package KotlinCore
 
+data class Person(var name: String = "", var age: Int = 0)
+
+
 fun main() {
-
-    calculate(
-        result = {
-
-        }, onItemClick = {
-
-        }
-    )
-
-    calculate(
-        onItemClickLister = object : OnItemClickLister {
-            override fun invoke(): String {
-                return "salom"
-            }
-        }, result = object : Result {
-            override fun invoke() {
-
-            }
-        })
-
-
-    val item = "sakim"
-    item.replace()
-
-}
-
-fun calculate(
-    result: (String) -> Unit,
-    onItemClick: () -> Unit
-) {
-    result("salom")
-    onItemClick()
-}
-
-fun calculate(): (String) -> Int {
-    return { 5 }
-}
-
-
-interface OnItemClickLister {
-    operator fun invoke(): String
-}
-
-interface Result {
-    operator fun invoke()
-}
-
-fun calculate(
-    onItemClickLister: OnItemClickLister,
-    result: Result
-) {
-    onItemClickLister.invoke()
-    result.invoke()
-}
-
-fun String.replace(): String {
-    return this.reversed()
-}
-
-fun replace(item: String): String {
-    return item.reversed()
+    var person = Person(name = "nur")
+    println(person)
+    person = person.copy(age = 1)
+    println(person)
 }
---
+++ src/Map2/WordAppend.java
@@ -0,0 +1,13 @@
+package Map2;
+
+
+import java.util.HashMap;
+
+public class WordAppend {
+    public String wordAppend(String[] strings) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        return "map";
+    }
+}
