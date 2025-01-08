626057436b51305d10df04312bb1f117e9878a9f
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-08 13:40:18 +0500

    Functional1

---
+++ src/Functional1/Square.java
@@ -0,0 +1,10 @@
+package Functional1;
+
+import java.util.List;
+
+public class Square {
+    public List<Integer> square(List<Integer> nums) {
+        nums.replaceAll(n -> n * n);
+        return nums;
+    }
+}
