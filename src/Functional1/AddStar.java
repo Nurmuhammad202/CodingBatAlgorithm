bedb8090012b69e9c2f8841da4c6481775b3382b
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-08 13:41:45 +0500

    Functional1

---
+++ src/Functional1/AddStar.java
@@ -0,0 +1,10 @@
+package Functional1;
+
+import java.util.List;
+
+public class AddStar {
+    public List<String> addStar(List<String> strings) {
+        strings.replaceAll(n -> n + "*");
+        return strings;
+    }
+}
