dce0e634f596a314f3d345e469b60031a79fa174
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-10 14:11:40 +0500

    Functional1

---
+++ src/Functional1/Lower.java
@@ -0,0 +1,12 @@
+package Functional1;
+
+import java.util.List;
+
+public class Lower {
+    public List<String> lower(List<String> strings) {
+
+        strings.replaceAll(String::toLowerCase);
+
+        return strings;
+    }
+}
