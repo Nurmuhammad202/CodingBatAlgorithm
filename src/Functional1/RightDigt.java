50bcbb8d410bbe9a2034b651d0c791cd771b7066
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-10 14:10:32 +0500

    Functional1

---
+++ src/Functional1/RightDigt.java
@@ -0,0 +1,12 @@
+package Functional1;
+
+import java.util.List;
+
+public class RightDigt {
+    public List<Integer> rightDigit(List<Integer> nums) {
+
+        nums.replaceAll(it -> it % 10);
+
+        return nums;
+    }
+}
