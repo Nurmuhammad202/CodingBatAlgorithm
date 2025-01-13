29e167351af39bf89ebd6ee233212f18f2eb4f70
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-13 17:22:56 +0500

    Functional2

---
+++ src/fun2/NoNeg.java
@@ -0,0 +1,12 @@
+package fun2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NoNeg {
+    public List<Integer> noNeg(List<Integer> nums) {
+        return nums.stream()
+                .filter(num -> num >= 0)
+                .collect(Collectors.toList());
+    }
+}
