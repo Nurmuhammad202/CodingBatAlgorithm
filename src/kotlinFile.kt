38246044a4afdb91daf638df0ac4e4f45f8bffed
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-15 16:42:46 +0500

    Functional2

--- src/kotlinFile.kt
+++ src/kotlinFile.kt
@@ -1,10 +1,32 @@
-fun main() {
-    val name = "Dilshod"
-    val name2 = "Dilshod"
+import javax.lang.model.type.UnionType
+import kotlin.random.Random
 
-    println(name == name2)        //true
-    println(name === name2)       //false
-    println(name.equals(name2))   //true
+inline fun minus(
+    noinline action: (Int) -> Unit,
+    calculate: (String) -> Unit,
+    listener: (String) -> Unit
+) {
+    action(read())
+    calculate("Hello")
 
-
+    listener("listener")
+//    Thread {
+//        listener("Hello")
+//    }.start()
 }
+
+fun read(): Int {
+    return Random.nextInt();
+}
+
+fun main() {
+    minus(action = {
+        println(it)
+    }, calculate = {
+        println(it)
+    }, listener = {
+        println(it)
+        return
+    })
+}
+
