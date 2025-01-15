44f39f84073be4320192d8325b66f7d06aaeca3a
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-15 17:03:09 +0500

    Functional2

---
+++ src/fun2/Square56.java
@@ -0,0 +1,13 @@
+package fun2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Square56 {
+    public List<Integer> square56(List<Integer> nums) {
+        return nums.stream()
+                .map(it -> (it * it) + 10)
+                .filter(num -> num % 10 != 6 && num % 10 != 5)
+                .collect(Collectors.toList());
+    }
+}
