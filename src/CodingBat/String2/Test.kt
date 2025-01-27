c6c48f5e79523b03b49f3e3de7ffbfd5a596437e
Yuldoshev Nurnuhammad <86513711+Nurmuhammad202@users.noreply.github.com>
2025-01-27 13:00:56 +0500

    scoresIncreasing

--- src/CodingBat/String2/Test.kt
+++ src/CodingBat/String2/Test.kt
@@ -1,11 +1,7 @@
 package CodingBat.String2
 
-import java.io.File
-import java.io.FileReader
-
-
-fun main() {
-    val file = File("example.txt")
-    val reader = FileReader(file) // IOException yuzaga kelishi mumkin
-    println(reader.readText())
+class Point(val x: Int, val y: Int) {
+    operator fun minus(other: Point): Boolean {
+        return false
+    }
 }
